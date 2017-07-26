cb26f9e0b5b576461de2cbc35f256ff3e1dc6770
jianmyphy123 <jianmyphy123@tutanota.com>
2017-07-26 18:20:07 +0200

    init

--- app.js
+++ app.js
@@ -105,4 +105,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+// app.listen(3000, process.env.IP, function(){
+//     console.log("Server has started");
+// });
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server has started");
+});
