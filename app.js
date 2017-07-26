c20ed7956966511968a1b93ee61379ce86293f8f
jianmyphy123 <jianmyphy123@tutanota.com>
2017-07-26 18:03:12 +0200

    init

---
+++ app.js
@@ -0,0 +1,108 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// Connect To Database
+mongoose.connect(config.database);
+
+// On Connection
+mongoose.connection.on('connected', () => {
+  console.log('Connected to database ' + config.database);
+});
+
+mongoose.connection.on('error', (err) => {
+  console.log('Database ' + err);
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+app.use(cookieParser());
+
+app.use(session({
+	secret: 'secret',
+	saveUninitialized: true,
+	resave: true
+}));
+
+app.use(flash());
+app.use(function(req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(req, res) {
+   // Expose "error" and "message" to all views that are rendered.
+   res.locals.error = req.session.error || '';
+   res.locals.message = req.session.message || '';
+
+   // Remove them so they're not displayed on subsequent renders.
+   delete req.session.error;
+   delete req.session.message;
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,170 @@
+var express = require('express');
+const bcrypt = require('bcrypt');
+var router = express.Router();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user');
+const nodemailer = require('nodemailer');
+const mailconfig = require('../config/mail');
+const secret = require('../config/secret').value;
+const domain = require('../config/conf').domain;
+
+
+var transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: mailconfig.username,
+    pass: mailconfig.password
+  }
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('users/signup', {
+    title: 'Sign Up'
+  });
+});
+
+router.post('/signup', function(req, res) {
+
+  var firstname = req.body.user_first_name;
+  var lastname = req.body.user_last_name;
+	var email = req.body.user_email;
+	var company = req.body.user_company;
+	var jobfunction = req.body.job_function;
+	var password = req.body.user_password;
+	var password2 = req.body.user_confirm_password;
+
+  req.checkBody('user_first_name', 'Firstname field is required').notEmpty();
+  req.checkBody('user_last_name', 'Lastname field is required').notEmpty();
+	req.checkBody('user_email', 'Email field is required').notEmpty();
+	req.checkBody('user_email', 'Email not valid').isEmail();
+	req.checkBody('user_password', 'Password field is required').notEmpty();
+	req.checkBody('user_confirm_password', 'Password do not match').equals(req.body.user_password);
+
+  let errors = null;
+
+  req.getValidationResult().then(function(result) {
+    if (!result.isEmpty()) {
+
+      errors = result.array();
+
+      res.render('users/signup', {
+  			errors: errors,
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        company: company,
+        jobfunction: jobfunction
+  		});
+    } else {
+
+      let temporaryToken = bcrypt.hashSync(email, 10);
+
+  		var newUser = new User({
+        firstname: firstname,
+        lastname: lastname,
+        email: email,
+        password: password,
+        company: company,
+        jobfunction: jobfunction,
+        temporaryToken: temporaryToken
+  		});
+
+  		// Create User
+  		User.createUser(newUser, function(err, user) {
+  			if(err) console.log(err);
+  			console.log(user);
+  		});
+
+  		// Success Message
+  		req.flash('success', 'Account registered! Please check your e-mail for activation link');
+
+      var mainOptions = {
+        from: 'John Doe <johndoe@outlook.com>',
+        to: email,
+        subject: domain + ' Activation Link',
+        text: 'Hello '+firstname+',Thank you for your registering at '+ domain +'. Please click on the link to complete your activation: http://'+domain+'/activate/'+temporaryToken,
+        html: 'Hello <strong>'+firstname+'</strong>,<br><br>Thank you for your registering at localhost.com. Please click on the link to complete your activation: <br><br><a href="http://'+domain+'/activate/'+temporaryToken+'">http://'+domain+'/activate</a>'
+      }
+
+      transporter.sendMail(mainOptions, function(error, info) {
+        if(error) {
+          console.log(error);
+        }
+        else {
+          console.log('Message Sent: '+info.response);
+          res.render('users/activation_sent');
+        }
+      });
+
+  	}
+  });
+
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('users/login', {
+    title: 'Log In'
+  });
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.getUserById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy({
+    usernameField: 'user_login',
+    passwordField: 'user_password'
+  },
+	function(email, password, done) {
+		User.getUserByEmail(email, function(err, user) {
+			if(err) console.log(err);
+			if(!user) {
+				console.log('Unknown User');
+				return done(null, false, { message: 'Unknown User' });
+			}
+			User.comparePassword(password, user.password, function(err, isMatch) {
+				if(err) console.log(err);
+				if(isMatch) {
+          if(!user.active) {
+            console.log('Not activated yet');
+            return done(null, false, { message: 'Your account is not yet activated. Please check your e-mail for activation link.'});
+          }
+					else
+            return done(null, user);
+				} else {
+					console.log('Invalid Password');
+					return done(null, false, { message: 'Invalid Password'});
+				}
+			});
+		}); 
+	}
+));
+
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+	failureRedirect: '/users/login',
+  failureFlash: true})
+);
+
+
+
+router.get('/reset_password', function(req, res, next) {
+  res.render('users/reset_password', {
+    title: 'Reset password'
+  });
+});
+
+router.get('/reset_password_sent', function(req, res, next) {
+  res.render('users/reset_password_sent', {
+    title: 'Reset password | Instructions sent to email'
+  });
+});
+
+module.exports = router;
